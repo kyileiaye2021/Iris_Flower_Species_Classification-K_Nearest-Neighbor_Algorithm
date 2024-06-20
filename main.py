56a39412f1a118040f8c2c26cce77e4604146a5d
aye, kyi lei <kaye2@go.pasadena.edu>
2024-06-20 00:36:19 -0700

    Adding userinput and predict the species of the flower

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ from sklearn.metrics import confusion_matrix, f1_score, accuracy_score  #for tes
 #loading the dataset
 data = pd.read_csv("IRIS.csv")
 df = pd.DataFrame(data)
+print("==========================Iris Flower Dataset========================")
 print(df.head())
 print(len(df))
+print()
 
 #========================Preparing the dataset===========================
 #Checking and replacing the cells that are 0 with the mean of the column data
@@ -40,20 +42,22 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 #Calculate k value
-k = int(np.sqrt(len(X_train_scaled))) #square root heuristic
+k = int(np.sqrt(len(X_train_scaled))) #using square root heuristic
 print("The number of neighbors: ", k)
+
 #--------------------Fitting the model---------------------
 #Define the KNN classifier model
 classifier = KNeighborsClassifier(n_neighbors=k-1, p=2, metric="euclidean")
 classifier.fit(X_train_scaled, y_train)
 y_pred = classifier.predict(X_test_scaled)
-print(y_pred)
-
+print()
 
 #-------------------Evaluating the model with performance metrics-------------------
+print("===============Evaluating the KNN Model================")
 #Confusion Matrix
 #A confusion matrix with no misclassifications would indicate that the model is performing perfectly on the given dataset, with 100% accuracy for all classes. 
 cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
 print(cm)
 
 #f-1 score
@@ -69,3 +73,16 @@ print("F1-score (weighted):", f1_score(y_test, y_pred, average='weighted'))
 #accuracy score
 accuracy_score_of_model = accuracy_score(y_test, y_pred)
 print("Accuracy Score:", accuracy_score_of_model)
+print()
+
+#===========================Predicting User Input=======================
+print("==============Predicting Species of Input Iris Flower=============")
+sepal_length = float(input("Sepal Length: "))
+sepal_width = float(input("Sepal Width: "))
+petal_length = float(input("Petal Length: "))
+petal_width = float(input("Petal Width: "))
+
+user_input = [[sepal_length, sepal_width, petal_length, petal_width]]
+user_input_pred = classifier.predict(user_input)
+print(f"The species of the given iris flower is predicted to be {user_input_pred[0]}!") #return index 0 var because predict() returns numpy array
+print()
